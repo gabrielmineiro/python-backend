97354f00c2d2664002018939ee8768e04c08676c
gabriel <gabrielmineironogueira@gmail.com>
2023-01-26 09:22:26 -0300

    finalizado

--- _core/settings.py
+++ _core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "cnab",
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = "../styles/home.css/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- _core/urls.py
+++ _core/urls.py
@@ -2,10 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
-from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("cnab.urls"))
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    path("", include("cnab.urls")),
+] 
